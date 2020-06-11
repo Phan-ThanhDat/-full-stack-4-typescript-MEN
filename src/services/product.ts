31ae0bc7ebe221b31172271fa841481a9e8a4e25
hongduc-phan <phduc.phanhongduc@gmail.com>
2020-06-12 01:12:40 +0300

    pagination get all products and get products  by query params

--- src/services/product.ts
+++ src/services/product.ts
@@ -1,12 +1,31 @@
 import Product, { ProductType } from '../models/Product'
 import { Request, Response, NextFunction } from 'express'
 
-function findAll(): Promise<ProductType[]> {
-  return Product.find().sort({ name: 1 }).exec() // Return a Promise
+function findAll(req: Request): Promise<ProductType[]> {
+  const page = req.query.page || 1
+  const limit = req.query.limit || 10
+  const pageOptions = {
+    page: (page as number) || 0,
+    limit: limit as number,
+  }
+  return Product.find()
+    .skip(pageOptions.page * pageOptions.limit)
+    .limit(pageOptions.limit)
+    .sort({ name: 1 })
+    .exec() // Return a Promise
 }
 
 function findProductByQueryParams(req: Request): Promise<ProductType[]> {
-  return Product.find(req.query).exec()
+  const page = req.query.page || 1
+  const limit = req.query.limit || 10
+  const pageOptions = {
+    page: (page as number) || 0,
+    limit: limit as number,
+  }
+  return Product.find(req.query)
+    .skip(pageOptions.page * pageOptions.limit)
+    .limit(pageOptions.limit)
+    .exec()
 }
 
 function findProductById(id: string): Promise<ProductType | null> {
